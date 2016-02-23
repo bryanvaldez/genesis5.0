a264b6c66cb89f9c36a4c86f4b730730d7d42f45
Bryan Valdez <ibryan.valdez@gmail.com>
2016-02-23 11:27:13 -0500

    add routes

---
+++ app/Http/Controllers/TicketsController.php
@@ -0,0 +1,12 @@
+<?php namespace genesis50\Http\Controllers;
+
+use genesis50\Http\Requests;
+use genesis50\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class TicketsController extends Controller {
+
+	//
+
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,40 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', [
 
-Route::get('home', 'HomeController@index');
+	'as'   	=> 	'tickets.latest',
+	'uses' 	=> 	'TicketsController@latest'
+
+]);
+
+Route::get('/populares', [
+
+	'as'   	=> 	'tickets.popular',
+	'uses' 	=> 	'TicketsController@popular'
+
+]);
+
+Route::get('/pendientes', [
+
+	'as'   	=> 	'tickets.open',
+	'uses' 	=> 	'TicketsController@open'
+
+]);
+
+Route::get('/tutoriales', [
+
+	'as'   	=> 	'tickets.closed',
+	'uses' 	=> 	'TicketsController@closed'
+
+]);
+
+Route::get('/solicitud/(id)', [
+
+	'as'   	=> 	'tickets.details',
+	'uses' 	=> 	'TicketsController@details'
+
+]);
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
